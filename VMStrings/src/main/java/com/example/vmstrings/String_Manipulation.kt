a085f470df06bfd39b6c7185be5602266e41ecdd
dhivya <dhivyachinnadurai4@gmail.com>
2023-03-24 00:04:44 +0530

    change

--- VMStrings/src/main/java/com/example/vmstrings/String_Manipulation.kt
+++ VMStrings/src/main/java/com/example/vmstrings/String_Manipulation.kt
@@ -2,23 +2,23 @@ package com.example.vmstrings
 
 
 object String_Manipulation {
-     val EMP= "This field is required"
-     val EQUALS = "This is palindrome"
-     val ERR = "This is not a palindrome"
+     val empty= "This field is required"
+     val palindrome = "This is palindrome"
+     val not_palindrome = "This is not a palindrome"
 
-    fun isPrime(n: Int): Boolean {
+    fun prime(n: Int): Boolean {
         if (n <= 1) return false
         for (i in 2 until n) if (n % i === 0) return false
         return true
     }
 
-    fun prime_index(input: String):String{
+    fun primeIndex(input: String):String{
 
         val n = input.length
 
         var output:String = " "
 
-        for (i in 1..n) if (!isPrime(i)){
+        for (i in 1..n) if (!prime(i)){
             val b: String = input[i-1].toString()
             val c = output
             output=c+b
@@ -33,14 +33,14 @@ object String_Manipulation {
     fun isPalindrome(input: String): String {
         val reverse = StringBuffer(input).reverse().toString()
         if(input.isEmpty()){
-            return EMP
+            return empty
         }
         if(input == reverse) {
-            return EQUALS +" \n"+"Output string is  - "+prime_index(input)
+            return palindrome +" \n"+"Output string is  - "+primeIndex(input)
 
 
         }else {
-            return ERR
+            return not_palindrome +" \n"+"Output string is  - "+primeIndex(input)
         }
 
     }
